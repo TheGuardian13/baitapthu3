21f30181a88a5f9d845bfab3541d2acf5d83b295
TheGuardian13 <tymt.diquabongtoi@gmail.com>
2025-09-09 17:52:33 +0700

    Đổi nền đen

---
+++ Hunggiaiphuongtrinh.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(841, 524);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtKQ;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnGiai;
+        private System.Windows.Forms.Label Ketqua;
+        private System.Windows.Forms.Label Nhapc;
+        private System.Windows.Forms.Label Nhapb;
+        private System.Windows.Forms.Label Nhapa;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label tieuDe;
+    }
+}
