b49306c8cace18ad04217b992ac07cdd1e4b9708
TheGuardian13 <tymt.diquabongtoi@gmail.com>
2025-09-16 08:41:05 +0700

    Bài tách tên

---
+++ Hungtachten.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace baitapthu3
+{
+    partial class Hungtachten
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtDem = new System.Windows.Forms.TextBox();
+            this.txtHo = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLamMoi = new System.Windows.Forms.Button();
+            this.btnPhanTach = new System.Windows.Forms.Button();
+            this.lblTen = new System.Windows.Forms.Label();
+            this.lblDem = new System.Windows.Forms.Label();
+            this.lblHo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblnNhapHoten = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTen
+            // 
+            this.txtTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtTen.Location = new System.Drawing.Point(256, 267);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.ReadOnly = true;
+            this.txtTen.Size = new System.Drawing.Size(343, 31);
+            this.txtTen.TabIndex = 23;
+            // 
+            // txtDem
+            // 
+            this.txtDem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtDem.Location = new System.Drawing.Point(256, 214);
+            this.txtDem.Name = "txtDem";
+            this.txtDem.ReadOnly = true;
+            this.txtDem.Size = new System.Drawing.Size(343, 31);
+            this.txtDem.TabIndex = 22;
+            // 
+            // txtHo
+            // 
+            this.txtHo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtHo.Location = new System.Drawing.Point(256, 160);
+            this.txtHo.Name = "txtHo";
+            this.txtHo.ReadOnly = true;
+            this.txtHo.Size = new System.Drawing.Size(343, 31);
+            this.txtHo.TabIndex = 21;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtHoTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtHoTen.Location = new System.Drawing.Point(256, 98);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(343, 31);
+            this.txtHoTen.TabIndex = 20;
+            this.txtHoTen.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtHoTen_KeyDown);
+            this.txtHoTen.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHoTen_KeyPress);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Yellow;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnExit.Location = new System.Drawing.Point(621, 375);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(128, 43);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnLamMoi
+            // 
+            this.btnLamMoi.BackColor = System.Drawing.Color.Yellow;
+            this.btnLamMoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnLamMoi.Location = new System.Drawing.Point(57, 375);
+            this.btnLamMoi.Name = "btnLamMoi";
+            this.btnLamMoi.Size = new System.Drawing.Size(128, 43);
+            this.btnLamMoi.TabIndex = 18;
+            this.btnLamMoi.Text = "Làm mới";
+            this.btnLamMoi.UseVisualStyleBackColor = false;
+            this.btnLamMoi.Click += new System.EventHandler(this.btnLamMoi_Click);
+            // 
+            // btnPhanTach
+            // 
+            this.btnPhanTach.BackColor = System.Drawing.Color.Yellow;
+            this.btnPhanTach.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnPhanTach.Location = new System.Drawing.Point(351, 375);
+            this.btnPhanTach.Name = "btnPhanTach";
+            this.btnPhanTach.Size = new System.Drawing.Size(128, 43);
+            this.btnPhanTach.TabIndex = 17;
+            this.btnPhanTach.Text = "Phân tách";
+            this.btnPhanTach.UseVisualStyleBackColor = false;
+            this.btnPhanTach.Click += new System.EventHandler(this.btnPhanTach_Click);
+            // 
+            // lblTen
+            // 
+            this.lblTen.AutoSize = true;
+            this.lblTen.BackColor = System.Drawing.Color.Cyan;
+            this.lblTen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblTen.Location = new System.Drawing.Point(52, 270);
+            this.lblTen.Name = "lblTen";
+            this.lblTen.Size = new System.Drawing.Size(49, 25);
+            this.lblTen.TabIndex = 16;
+            this.lblTen.Text = "Tên";
+            // 
+            // lblDem
+            // 
+            this.lblDem.AutoSize = true;
+            this.lblDem.BackColor = System.Drawing.Color.Cyan;
+            this.lblDem.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblDem.Location = new System.Drawing.Point(52, 217);
+            this.lblDem.Name = "lblDem";
+            this.lblDem.Size = new System.Drawing.Size(56, 25);
+            this.lblDem.TabIndex = 15;
+            this.lblDem.Text = "Đệm";
+            // 
+            // lblHo
+            // 
+            this.lblHo.AutoSize = true;
+            this.lblHo.BackColor = System.Drawing.Color.Cyan;
+            this.lblHo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblHo.Location = new System.Drawing.Point(52, 160);
+            this.lblHo.Name = "lblHo";
+            this.lblHo.Size = new System.Drawing.Size(39, 25);
+            this.lblHo.TabIndex = 14;
+            this.lblHo.Text = "Họ";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(277, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 25);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "PHẦM MỀM TÁCH HỌ TÊN";
+            // 
+            // lblnNhapHoten
+            // 
+            this.lblnNhapHoten.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblnNhapHoten.AutoSize = true;
+            this.lblnNhapHoten.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblnNhapHoten.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblnNhapHoten.Location = new System.Drawing.Point(52, 98);
+            this.lblnNhapHoten.Name = "lblnNhapHoten";
+            this.lblnNhapHoten.Size = new System.Drawing.Size(168, 25);
+            this.lblnNhapHoten.TabIndex = 12;
+            this.lblnNhapHoten.Text = "Nhập Họ và Tên";
+            // 
+            // Hungtachten
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtTen);
+            this.Controls.Add(this.txtDem);
+            this.Controls.Add(this.txtHo);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnLamMoi);
+            this.Controls.Add(this.btnPhanTach);
+            this.Controls.Add(this.lblTen);
+            this.Controls.Add(this.lblDem);
+            this.Controls.Add(this.lblHo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblnNhapHoten);
+            this.Name = "Hungtachten";
+            this.Text = "Hungtachten";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTen;
+        private System.Windows.Forms.TextBox txtDem;
+        private System.Windows.Forms.TextBox txtHo;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLamMoi;
+        private System.Windows.Forms.Button btnPhanTach;
+        private System.Windows.Forms.Label lblTen;
+        private System.Windows.Forms.Label lblDem;
+        private System.Windows.Forms.Label lblHo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblnNhapHoten;
+    }
+}
